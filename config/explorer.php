5645cb1ec3fb7d97d00653086179cbd589d60a7b
Kieran Brown <kswb96@gmail.com>
2026-09-18 18:01:25 +0100

    Tidy

--- config/explorer.php
+++ config/explorer.php
@@ -16,7 +16,7 @@ return [
 
     'disks' => array_values(array_filter(array_map(
         trim(...),
-        explode(',', (string) env('EXPLORER_DISKS', 's3-files,s3')),
+        explode(',', (string) env('EXPLORER_DISKS', 'local,public')),
     ), fn (string $disk): bool => $disk !== '')),
 
 ];
